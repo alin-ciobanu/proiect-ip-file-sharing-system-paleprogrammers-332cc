2f7c498a941ef950926d666e5e059f252d5bd60b
alin.ciobanu <alin.ciobanu@cti.pub.ro>
2014-05-16 21:08:15 +0300

    merge sa transferi fisier cu fisier

--- src/client/logic/ClientToClientRequestSolverThread.java
+++ src/client/logic/ClientToClientRequestSolverThread.java
@@ -34,46 +34,43 @@ public class ClientToClientRequestSolverThread extends Thread {
             e.printStackTrace();
         }
 
-        while (true) {
+        ClientToClientRequest clientToClientRequest = null;
 
-            ClientToClientRequest clientToClientRequest = null;
+        try {
+            clientToClientRequest = (ClientToClientRequest) objectInputStream.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
+        System.out.println("got req " + clientToClientRequest);
+
+        if (clientToClientRequest.getCode() == ClientToClientRequest.TRANSFER) {
+
+            String filename = clientToClientRequest.getFilename();
+            File file = new File(filename);
+
+            ClientToClientResponse response = new ClientToClientResponse();
+            response.setFileLength(file.length());
             try {
-                clientToClientRequest = (ClientToClientRequest) objectInputStream.readObject();
+                response.setFile(FileUtils.readFileToByteArray(file));
             } catch (IOException e) {
                 e.printStackTrace();
-            } catch (ClassNotFoundException e) {
+            }
+
+            try {
+                objectOutputStream.writeObject(response);
+            } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            System.out.println("got req " + clientToClientRequest);
-
-            if (clientToClientRequest.getCode() == ClientToClientRequest.TRANSFER) {
-
-                String filename = clientToClientRequest.getFilename();
-                File file = new File(filename);
-
-                ClientToClientResponse response = new ClientToClientResponse();
-                response.setFileLength(file.length());
-                try {
-                    response.setFile(FileUtils.readFileToByteArray(file));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                try {
-                    objectOutputStream.writeObject(response);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+
 
         }
     }
